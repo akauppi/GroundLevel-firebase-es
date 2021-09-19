004d45fc3fcef66915990178cbfb4fae211884c9
Asko Kauppi <akauppi@gmail.com>
2021-09-19 15:26:10 +0300

    Fixed a bug in Vite mode detection

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -14,11 +14,6 @@ import vue from '@vitejs/plugin-vue'
 const myPath = dirname(fileURLToPath(import.meta.url))
 const srcPath = pJoin(myPath, 'src');
 
-const DEV = process.env["NODE_ENV"] !== "production";
-  //
-  // true:  hosting, on-the-fly 🪰
-  // false: building for shipment; 'dist/'; no 'vitebox' files are needed!! 🚢
-
 /*
 * For an absolute path 'p', provide the immediate subdirectories within it.
 */
@@ -53,7 +48,7 @@ const forcedVueComponents = new Set([
 /*
 * Map '@ops/goo' to 'vitebox/ops/goo.js' (for 'npm run dev:...' only!)
 */
-const opsAliases_DEV = DEV && (() => {
+function opsAliases_DEV() {
   const opsPath = pJoin(myPath, 'vitebox/ops');
 
   const pairs = readdirSync(opsPath).map( s => {    // e.g. 'central.js'
@@ -65,7 +60,7 @@ const opsAliases_DEV = DEV && (() => {
   }).filter( x => x !== undefined ); // Array of ['@ops/...', '...path...']
 
   return Object.fromEntries(pairs);
-})();
+}
 
 // Help Rollup in packaging.
 //
@@ -117,106 +112,103 @@ const chunkTo = [     // Array of Regex
   // Firebase is marked as a peer dependency, and provided by the upper level.
 ];
 
-export default {
+function configGen({ _ /*command*/, mode }) {
+  //console.log("!!!", {command, mode});    // "serve"|"build", "dev_local"|"development"|"production"
 
-  ...(DEV ? {    // 'npm run dev:{local|online}'
-    // Decides where 'index.html' is (and root for other dir configs)
-    root: 'vitebox',
-    envDir: '..',     // actual 'app' dir
+  const DEV_MODE = (mode === 'production') ? null :
+    (mode === 'dev_local' && 'local') || (mode === 'development' && 'online') || fail(`Unexpected mode: ${mode}`);
 
-    // With 2.4.x, this was a way to point up from 'vitebox', but creates lots of warnings on 2.5.6:
-    //  <<
-    //    files in the public directory are served at the root path.
-    //    Instead of /@vite/client, use /@vite/client.
-    //  <<
-    //
-    // ..so replaced with a symbolic link from 'vitebox/public' -> '../public'.
-    //
-    //publicDir: '../public'    // relative to 'vitebox'
-  } : {
-    // regular root
-  }),
+  return {
+    ...(DEV_MODE ? {    // 'npm run dev:{local|online}'
+      // Decides where 'index.html' is (and root for other dir configs)
+      root: 'vitebox',
+      envDir: '..',     // actual 'app' dir
 
-  resolve: {
-    alias: { ...subAliases,
-      '/@': srcPath,
-      ...(opsAliases_DEV || {}),
+      // With 2.4.x, this was a way to point up from 'vitebox', but creates lots of warnings on 2.5.6:
+      //  <<
+      //    files in the public directory are served at the root path.
+      //    Instead of /@vite/client, use /@vite/client.
+      //  <<
+      //
+      // ..so replaced with a symbolic link from 'vitebox/public' -> '../public'.
+      //
+      //publicDir: '../public'    // relative to 'vitebox'
+    } : {
+      // regular root
+    }),
+
+    resolve: {
+      alias: {
+        ...subAliases,
+        '/@': srcPath,
+        ...(DEV_MODE ? opsAliases_DEV() : {}),
+      }
     },
 
-    // Let's stick to *only* 'module' for a while; this can be completely removed once we know things are stable.
-    //
-    // Until Firebase 9.0.0-beta.1, we needed to override the 'mainFields' (since 'auth' was not providing a "module"
-    // field). Now the Vite defaults are fine.
-    //
-    // With 9.0.0-beta.1, '@firebase/...' packaging varies from subpackage to subpackage, but they all now carry "module"
-    // which is enough (and part of Vite default 'resolve.mainFields' list).
-    //
-    // With 9.0.0-beta.7, auth started working when this setting was disabled.
-    //
-    //mainFields: ["module"]    // KEEP for a while
-  },
+    build: {
+      ...(!DEV_MODE ? {
+        //outDir: "dist"    // also the default (relative to 'app' folder)
+      } : {}),
 
-  // Means to pass build time values to the browser
-  //
-  // Note: IF using in production, one needs to provide eg. the quotes, since it's a literal replace. This makes sense
-  //    for using the mechanism as a macro (to inject code). Not so nice for strings.
-  //
-  define: {
-    "_VERSION": "\"0.0.0\""
-  },
+      minify: false,
+      sourcemap: true,    // "generate production source maps"    tbd. do we need them for local development (or does Vite always provide them?); does ops create them, anyways?
+      target: 'esnext',   // assumes native dynamic imports (default for Vite 2.3.0+)
+      //polyfillDynamicImport: false
 
-  build: {
-    ...(!DEV ? {
-      //outDir: "dist"    // also the default (relative to 'app' folder)
-    }:{}),
+      // Expose the code side, with a predictable export path.
+      lib: {
+        entry: path.resolve(srcPath, 'app.js'),
+        formats: ['es']   // internal use only (ESM; no 'name' field needed)
+      },
 
-    minify: false,
-    sourcemap: true,    // "generate production source maps"    tbd. do we need them for local development (or does Vite always provide them?); does ops create them, anyways?
-    target: 'esnext',   // assumes native dynamic imports (default for Vite 2.3.0+)
-    //polyfillDynamicImport: false
+      rollupOptions: {
+        external: [
+          /^@?firebase\//,    // don't try packing these - 'app-deploy-ops' provides them (of the same version)
+          //"/favicon.png"
+          ...(DEV_MODE ? [] : ['@ops/central', '@ops/perf'])
+        ],
+        output: {manualChunks}
+      },
 
-    // Expose the code side, with a predictable export path.
-    lib: {
-      entry: path.resolve(srcPath, 'app.js'),
-      formats: ['es']   // internal use only (ESM; no 'name' field needed)
+      // Note:
+      //    Documentation says 'true' would have: "..CSS imported in async chunks will be inlined into the async chunk
+      //    itself and inserted when the chunk is loaded."
+      //
+      //    This is not so (Vite 2.0.5). Why is 'app.css' there, then? (expecting the CSS to be baked into .js)
+      //
+      //cssCodeSplit: true,   // true: creates 'app.css'
+      cssCodeSplit: false,   // false: creates 'style.css'
     },
 
-    rollupOptions: {
-      external: [
-        /^@?firebase\//,    // don't try packing these - 'app-deploy-ops' provides them (of the same version)
-        //"/favicon.png"
-        ...(DEV ? [] : ['@ops/central', '@ops/perf'])
-      ],
-      output: { manualChunks }
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: tag => tag.includes('-') && !forcedVueComponents.has(tag)
+          }
+        }
+      })
+    ],
+
+    server: {
+      // Vite 2.3.1 (still in 2.4.4): "The fsServe restrictions are going to be enabled by default in a future version."
+      //    -> https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md#231-2021-05-12
+      fs: {
+        strict: true    // restrict access to the work directory
+      },
+
+      port: DEV_MODE === 'local' ? 3000:3001,
+      strictPort: true,
+
+      // Allows viewing from other devices, eg. a tablet.
+      host: true
     },
 
-    // Note:
-    //    Documentation says 'true' would have: "..CSS imported in async chunks will be inlined into the async chunk
-    //    itself and inserted when the chunk is loaded."
-    //
-    //    This is not so (Vite 2.0.5). Why is 'app.css' there, then? (expecting the CSS to be baked into .js)
-    //
-    //cssCodeSplit: true,   // true: creates 'app.css'
-    cssCodeSplit: false,   // false: creates 'style.css'
-  },
-
-  plugins: [
-    vue({ template: { compilerOptions: {
-      isCustomElement: tag => tag.includes('-') && !forcedVueComponents.has(tag)
-    }}})
-  ],
-
-  server: {
-    // Vite 2.3.1 (still in 2.4.4): "The fsServe restrictions are going to be enabled by default in a future version."
-    //    -> https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md#231-2021-05-12
-    fs: {
-      strict: true    // restrict access to the work directory
-    },
-
-    // Allows viewing from other devices, eg. a tablet.
-    host: true
-  },
-
-  // Clearing the screen is considered distracting, though one can PgUp to see what was there just prior to Vite launching.
-  clearScreen: false
+    // Clearing the screen is considered distracting, though one can PgUp to see what was there just prior to Vite launching.
+    clearScreen: false
+  }
 }
+
+function fail(msg) { throw new Error(msg) }
+
+export default configGen
